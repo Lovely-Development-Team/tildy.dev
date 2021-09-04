78e5834f6ca9c00240ef23e59abfeadb8261d762
Justin Hamilton <jwhamilton99@gmail.com>
2021-09-04 21:21:32 +0000

    improved async data fetching

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,59 +1,59 @@
-function setMembersList(membersString) {
-	document.getElementById("members").innerHTML = membersString;
+let membersURL = "/data/members.json";
+let projectsURL = "/data/projects.json";
+
+function getData(url) {
+	return new Promise((resolve, reject) => {
+		fetch(url)
+			.then(response => {
+				let text = response.text();
+				resolve(text);
+			})
+	})
 }
 
-function setProjectsList(nodes) {
-	var list = document.getElementById("projects");
-	for(node of nodes) {
-		list.appendChild(node);
-	}
-}
-
-function getProjectsFromData() {
-	let req = new XMLHttpRequest();
-	req.open("GET", "/data/projects.json", true);
-	req.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			var responseObject = JSON.parse(this.response);
-			var projects = responseObject["projects"];
-			var nodes = [];
-			
-			for(var i = 0; i < projects.length; i++) {
-				var project = projects[i];
-				var node = document.createElement("li");
-				node.innerHTML = ("<a href=\""+project["link"]+"\">"+project["name"]+"</a>")
-				nodes.push(node);
-			}
-			setProjectsList(nodes);
+function getProjects() {
+	getData(projectsURL).then(response => {
+		let responseObject = JSON.parse(response);
+		let projects = responseObject["projects"];
+		var nodes = [];
+		
+		for(var i = 0; i < projects.length; i++) {
+			let project = projects[i];
+			var li = document.createElement("li");
+			var a = document.createElement("a");
+			a.href = project["link"];
+			a.innerHTML = project["name"];
+			li.appendChild(a);
+			nodes.push(li);
 		}
-	}
-	req.send();
+		
+		let list = document.getElementById("projects");
+		for(node of nodes) {
+			list.appendChild(node);
+		}
+	});
 }
 
-function getMembersFromData() {
-	let req = new XMLHttpRequest();
-	req.open("GET", "/data/members.json", true);
-	req.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			var responseObject = JSON.parse(this.response);
-			var members = responseObject["members"]
-			var outputString = "We are "	
-			
-			for(var i = 0; i < members.length; i++) {
-				var member = members[i];
-				if(i == members.length-1) {
-					outputString+=("and <a href=\""+member["link"]+"\">"+member["name"]+"</a>.");
-				} else {
-					outputString+=("<a href=\""+member["link"]+"\">"+member["name"]+"</a>, ");
-				}
+function getMembers() {
+	getData(membersURL).then(response => {
+		let responseObject = JSON.parse(response);
+		let members = responseObject["members"];
+		var outputString = "We are ";
+		
+		for(var i = 0; i < members.length; i++) {
+			let member = members[i];
+			if(i == members.length-1) {
+				outputString+=("and <a href=\""+member["link"]+"\">"+member["name"]+"</a>.");
+			} else {
+				outputString+=("<a href=\""+member["link"]+"\">"+member["name"]+"</a>, ");
 			}
-			setMembersList(outputString);
 		}
-	}
-	req.send();
+		
+		document.getElementById("members").innerHTML = outputString;
+	});
 }
 
 function pageLoaded() {
-	getMembersFromData();
-	getProjectsFromData();
+	getMembers();
+	getProjects();
 }
